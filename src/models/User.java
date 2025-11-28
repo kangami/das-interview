2dd39e2f37668092225e4f82495969b91775a2ee
Karim Gatien <ngamikarim13@gmail.com>
2025-11-28 07:53:39 -0500

    adding user class description

--- src/models/User.java
+++ src/models/User.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Date;
+
 /**
  *  The User Class
  *  content user info
@@ -7,5 +9,33 @@ package models;
  *  @author KARIM NGAMI TEUMI
  */
 public class User {
+	
+	private final long id;
+	private final String firstName;
+	private final String lastName;
+	private final Date creationDate;
+	
+	public User(long id, String firstName, String lastName, Date creationDate) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.creationDate = creationDate;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
 	
 }
